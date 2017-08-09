2f11e6d328ea20988c32a1bd86945d6cff901e47
kyuja79 <kyuja79@gmail.com>
2017-08-09 10:30:35 +0900

    IRestTempalte 구현

--- restTemplateUtil/src/main/java/com/gaebalje/restTemplate/common/SearchAPIRestTemplateUtil.java
+++ restTemplateUtil/src/main/java/com/gaebalje/restTemplate/common/SearchAPIRestTemplateUtil.java
@@ -1,43 +1,44 @@
 package com.gaebalje.restTemplate.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestOperations;
 
 public class SearchAPIRestTemplateUtil extends RestOperationImpl implements IRestTemplate {
-
-	public SearchAPIRestTemplateUtil(RestOperations restOperation, String property, String property2) {
-		// TODO Auto-generated constructor stub
+	
+	private static Logger logger = LoggerFactory.getLogger(SearchAPIRestTemplateUtil.class);
+	
+	public SearchAPIRestTemplateUtil(RestOperations restOperation, String host, String port) {
+		this.restOperations = restOperation;
+		this.host = host;
+		this.port = port;
 	}
 
 	@Override
 	public <T> T get(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
-		return null;
+		return send(getCallUrl(url) + request, null, typeRef, HttpMethod.GET);
 	}
 
 	@Override
 	public <T> T post(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
-		return null;
+		return send(getCallUrl(url), request, typeRef, HttpMethod.POST);
 	}
 
 	@Override
 	public <T> T put(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
-		return null;
+		return send(getCallUrl(url), request, typeRef, HttpMethod.PUT);
 	}
 
 	@Override
 	public <T> T delete(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public <T> T send(String url, Object request, ParameterizedTypeReference<T> typeRef, HttpMethod httpMethod) {
-		// TODO Auto-generated method stub
-		return null;
+		return restOperation(logger, url, request, typeRef, httpMethod);
 	}
 
 }
--- restTemplateUtil/src/main/java/com/gaebalje/restTemplate/common/SsocioAPIRestTemplateUtil.java
+++ restTemplateUtil/src/main/java/com/gaebalje/restTemplate/common/SsocioAPIRestTemplateUtil.java
@@ -10,39 +10,53 @@ public class SsocioAPIRestTemplateUtil extends RestOperationImpl implements IRes
 
 	private static Logger logger = LoggerFactory.getLogger(SsocioAPIRestTemplateUtil.class);
 	
-	public SsocioAPIRestTemplateUtil(RestOperations restOperation, String property, String property2,
-			String property3) {
-		// TODO Auto-generated constructor stub
+	private String accessMember;
+	
+	public SsocioAPIRestTemplateUtil(RestOperations restOperation, String host, String port,
+			String accessMember) {
+		this.restOperations = restOperation;
+		this.host = host;
+		this.port = port;
+		this.accessMember = accessMember;
 	}
 
 	@Override
+	public String getCallUrl(String restUrl) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.host);
+		if(null != this.port && !"".equals(this.port)){
+			sb.append(":").append(this.port);
+		}
+		sb.append(restUrl);
+		if(null != this.accessMember && !"".equals(this.accessMember)){
+			sb.append("/").append(this.accessMember);
+		}
+		return sb.toString();
+	}
+	
+	@Override
 	public <T> T get(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
-		return null;
+		return send(getCallUrl(url) + request, null, typeRef, HttpMethod.GET);
 	}
 
 	@Override
 	public <T> T post(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
-		return null;
+		return send(getCallUrl(url), request, typeRef, HttpMethod.POST);
 	}
 
 	@Override
 	public <T> T put(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
-		return null;
+		return send(getCallUrl(url), request, typeRef, HttpMethod.PUT);
 	}
 
 	@Override
 	public <T> T delete(String url, Object request, ParameterizedTypeReference<T> typeRef) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public <T> T send(String url, Object request, ParameterizedTypeReference<T> typeRef, HttpMethod httpMethod) {
-		// TODO Auto-generated method stub
-		return null;
+		return restOperation(logger, url, request, typeRef, httpMethod);
 	}
 
 }
